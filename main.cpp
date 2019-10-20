9bc9c93755bcb6e14e9bfbbc68ee680c54c94f65
Fahim Salam Chowdhury <fahim.carbangla@gmail.com>
2019-10-20 14:23:09 +0600

    add vertex attribute

--- main.cpp
+++ main.cpp
@@ -44,6 +44,10 @@ int main(void)
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, (6 * sizeof(float)), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, (2 * sizeof(float)), (const void*)0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {

b202a3073d066c7741e288d1a760acd7b09a35c6
Fahim Salam Chowdhury <fahim.cse11@gmail.com>
2019-10-21 21:39:53 +0600

    shader in separate file

--- main.cpp
+++ main.cpp
@@ -2,6 +2,55 @@
 #include <GLFW/glfw3.h>
 #include "testLog.h"
 #include<iostream>
+#include<tuple>
+#include<fstream>
+#include<string>
+#include<sstream>
+
+//for getting current path
+#include<stdio.h>  /* defines FILENAME_MAX */
+// #define WINDOWS  /* uncomment this line to use it for windows.*/ 
+#ifdef WINDOWS
+    #include<direct.h>
+    #define GetCurrentDir _getcwd
+#else
+    #include<unistd.h>
+    #define GetCurrentDir getcwd
+#endif
+
+
+static std::string GetCurrentWorkingDir() {
+  char buff[FILENAME_MAX];
+  GetCurrentDir( buff, FILENAME_MAX );
+  std::string current_working_dir(buff);
+  return current_working_dir;
+}
+
+
+static std::tuple<std::string, std::string> parseShader(const std::string& filePath){
+    std::ifstream stream(filePath);
+
+    enum class ShaderType {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+    while (getline(stream,line))
+    {
+        if(line.find("#shader") != std::string::npos){
+            if(line.find("vertex") != std::string::npos)
+                type = ShaderType::VERTEX;
+            else if(line.find("fragment") != std::string::npos)
+                type = ShaderType::FRAGMENT;
+        } else if(type != ShaderType::NONE) {
+            ss[(int)type] << line << "\n";
+        }
+    }
+    stream.close();
+    return { ss[0].str(), ss[1].str() };
+}
 
 
 static unsigned int compileShader(unsigned int type, const std::string& source){
@@ -89,7 +138,7 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, (2 * sizeof(float)), (const void*)0);
 
-    std::string vertexShader = R"glsi(
+    /*std::string vertexShader = R"glsi(
         #version 330 core
 
         layout(location = 0) in vec4 position;
@@ -107,7 +156,9 @@ int main(void)
         void main(){
             color = vec4(1.0, 0.0, 0.0, 1.0);
         }
-    )glsi";
+    )glsi";*/
+
+    auto[vertexShader,fragmentShader] = parseShader(GetCurrentWorkingDir() + "/res/shaders/basic.shader");
 
     unsigned int shader = createShader(vertexShader,fragmentShader);
     glUseProgram(shader);
